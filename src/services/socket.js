708db186374d2fde86e67ce96eb6bc5370b1a993
Prasad528260 <prasadsubhedar5460@gmail.com>
2026-05-10 14:33:44 +0530

    socket

--- src/services/socket.js
+++ src/services/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
+  transports: ["polling", "websocket"],
+  upgrade: true,
 });
 
 export default socket;
